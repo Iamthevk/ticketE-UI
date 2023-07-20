8b15e6d14b3257c4328a89970e0a7a30bcc0e1c0
veekay17 <vijay.vuex@gmail.com>
2023-07-20 14:17:29 +0530

    code cleanup and added Navbar to page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "next/image";
+
+function Navbar() {
+  return (
+    <nav className="py-6 flex items-center justify-between">
+      <Image src="logo.svg" alt="logo" width={76} height={24} />
+      <div className="flex gap-2">
+        <Image src="question.svg" alt="logo" width={20} height={20} />
+        <span className="text-primary-500 hidden md:inline-block">Help</span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
